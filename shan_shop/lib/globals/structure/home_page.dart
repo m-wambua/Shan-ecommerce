2cecbcfcd3c12f4ccf425a5576f32d2c04f1403d
m-wambua <michaelmutisyawambua@gmail.com>
2024-02-21 13:14:17 +0300

    Added a search bar and menu items to the appBar

--- shan_shop/lib/globals/structure/home_page.dart
+++ shan_shop/lib/globals/structure/home_page.dart
@@ -10,6 +10,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isHomeHovered = false;
+  bool isShopHovered = false;
+  bool isCustomOrderHovered = false;
+  bool isAboutUsHovered = false;
+  bool isContactUsHovered = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +32,8 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         title: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             CircleAvatar(
               maxRadius: 26,
@@ -41,6 +48,123 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
+            const SizedBox(
+              width: 50,
+            ),
+            Expanded(
+                child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 8.0, vertical: 5.0),
+                  child: Container(height: 40,
+                     child: ClipRRect(borderRadius: BorderRadius.circular(30),
+                    child: TextField(
+                onChanged: (value) {
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 245, 245, 220),
+                  hintText: 'Search...',
+                  suffixIcon: IconButton(
+                    icon: const Icon(Icons.search),
+                    onPressed: () {},
+                  ),
+                  border: const OutlineInputBorder(),
+                ),
+              ),
+                  )
+                  
+                  )
+
+               
+            
+            )),
+            const Spacer(),
+            TextButton(
+                onPressed: () {},
+                onHover: (value) {
+                  setState(() {
+                    isHomeHovered = value;
+                  });
+                },
+                child: Text(
+                  'Home',
+                  style: TextStyle(
+                      color: isHomeHovered
+                          ? Color.fromARGB(255, 73, 2, 6)
+                          : Color.fromARGB(255, 245, 245, 220),),
+                )),
+            const SizedBox(
+              width: 10,
+            ),
+            TextButton(
+                onPressed: () {},
+                onHover: (value) {
+                  setState(() {
+                    isShopHovered = value;
+                  });
+                },
+                child: Text(
+                  'Shop Now',
+                  style: TextStyle(
+                      color: isShopHovered
+                          ? Color.fromARGB(255, 73, 2, 6)
+                          : Color.fromARGB(255, 245, 245, 220),),
+                )),
+            const SizedBox(
+              width: 10,
+            ),
+            TextButton(
+                onPressed: () {},
+                onHover: (value) {
+                  setState(() {
+                    isCustomOrderHovered = value;
+                  });
+                },
+                child: Text(
+                  'Custom Order',
+                  style: TextStyle(
+                      color: isCustomOrderHovered
+                          ? Color.fromARGB(255, 73, 2, 6)
+                          : Color.fromARGB(255, 245, 245, 220),),
+                )),
+            const SizedBox(
+              width: 10,
+            ),
+            TextButton(
+                onPressed: () {},
+                onHover: (value) {
+                  setState(() {
+                    isAboutUsHovered = value;
+                  });
+                },
+                child: Text(
+                  'About Us',
+                  style: TextStyle(
+                      color: isAboutUsHovered
+                          ? Color.fromARGB(255, 73, 2, 6)
+                          : Color.fromARGB(255, 245, 245, 220),),
+                )),
+            const SizedBox(
+              width: 10,
+            ),
+            TextButton(
+                onPressed: () {},
+                onHover: (value) {
+                  setState(() {
+                    isContactUsHovered = value;
+                  });
+                },
+                child: Text(
+                  'Contact Us',
+                  style: TextStyle(
+                      color: isContactUsHovered
+                          ? Color.fromARGB(255, 73, 2, 6)
+                          : Color.fromARGB(255, 245, 245, 220),),
+                )),
+                const SizedBox(width: 15,),
+                IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_bag))
           ],
         ),
       ),
@@ -48,7 +172,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           // Background CircleAvatar
           Positioned(
-    // Adjust as necessary
+            // Adjust as necessary
             child: Center(
               child: CircleAvatar(
                 radius: 300,
